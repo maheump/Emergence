3d8285dc667037e74acbc454e5953d5c8f2faa1e
Maxime Maheu <maheu.mp@gmail.com>
2018-04-22 19:21:03 +0200

    Analysis script: "Emergence_FTA_DistributionOfBel"

---
+++ Finger tracking analyses/Emergence_FTA_DistributionOfBeliefs.m
@@ -0,0 +1,217 @@
+% This scripts computes histograms of finger's positions inside the
+% triangular arena for the different types of sequences. Moreover, the
+% marginal histograms (along the relevant dimension) for the two types of
+% regularity (probabilistic and deterministic ones) are compared.
+%
+% Copyright (c) 2018 Maxime Maheu
+
+%% COMPUTE HISTOGRAMS OF FINGER'S POSITIONS
+%  ========================================
+
+% Get the different conditions
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+% Length of the sequences
+N = numel(G{1}.Seq);
+
+% Create indices...
+idxtrimap = cell(1,3);
+
+% For sequences entailing regularities
+for iHyp = 1:2
+    idxtrimap{iHyp} = cellfun(@(x) (x.Jump-1/2):N, G, 'UniformOutput', 0);
+    idxtrimap{iHyp}(setdiff(1:size(D,1), cidx{iHyp}),:) = {[]};
+end
+
+% For stochastic parts (both the fully-stochastic sequences and the
+% stochastic parts at the beginning of sequences with a regularity)
+idxtrimap{end} = cellfun(@(x) find(x.Gen == 1), G, 'UniformOutput', 0);
+
+% Define properties of the triangular histogram
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+% Triangle's specifications
+tcn = [0, sqrt(3)/2; 1, sqrt(3)/2; 1/2, 0];
+
+% Options for creating the density map
+j = 0.01;
+xgrid = tcn(1,1):j:tcn(2,1);
+ygrid = tcn(3,2):j:tcn(1,2);
+
+% Create a mask that specifies which cells of the matrices computed below
+% are outside the triangle
+ng = numel(xgrid);
+T = tril(ones(ng));
+T = [flip(T,2),T];
+mask = round(imresize(T, [round(ng/2*tan(pi/3)), ng]));
+
+% Options for the gaussian smooth
+filtSigma = 2;
+filtWidth = 2*ceil(2*filtSigma)+1;
+imageFilter = fspecial('Gaussian', filtWidth, filtSigma);
+
+% Perform the analysis
+% ~~~~~~~~~~~~~~~~~~~~
+
+% Get the number of triangular histogram maps we have to compute
+nMap = numel(idxtrimap);
+
+% Prepare output variables
+trajmap = cell(1,nMap+1);
+subpoints = cell(nSub,nMap);
+gppoints = cell(1,nMap);
+
+% For each density map to be created
+for iMap = 1:nMap
+
+    % Get the finger's positions in the current condition
+    points = cellfun(@(x,y) x.BarycCoord(y,:), ...
+        D, idxtrimap{iMap}, 'UniformOutput', 0);
+    for iSub = 1:nSub
+        subpoints{iSub,iMap} = cell2mat(points(:,iSub));
+    end
+    gppoints{iMap} = cell2mat(points(:));
+    
+    % Make sure it evolves between 0 and 1
+    gppoints{iMap}(gppoints{iMap} < 0) = 0;
+    gppoints{iMap}(gppoints{iMap} > 1) = 1;
+    
+    % Compute the density map
+    cc = gppoints{iMap}*tcn;
+    density = hist3(cc, 'Edges', {xgrid, ygrid})';
+    
+    % Smooth the density map
+    density = convn(density, imageFilter, 'same');
+
+    % Convert it to log scale
+    density = log(density+1);
+    
+    % Normalize it
+    trajmap{iMap+1} = density ./ sum(density(:));
+end
+
+% Also add a map of the difference
+trajmap{1} = (trajmap{3} ./ max(trajmap{3}(:))) ...
+           - (trajmap{2} ./ max(trajmap{2}(:)));
+       
+% Display triangular and marginal histrograms
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+% Prepare the window
+figure('Position', [1 805 800 300]);
+
+% Number of bins for the marginal histrograms
+nBin = 30;
+
+% For each density map
+for iMap = 1:nMap+1
+    sp = subplot(1, 4, iMap);
+
+    % Display the density maps of finger's position
+    imagesc(xgrid, ygrid, trajmap{iMap}, 'AlphaData', mask); hold('on');
+    contour(xgrid, ygrid, trajmap{iMap}, 5, 'k-', 'LineWidth', 1/2);
+    image(xgrid, ygrid, repmat(~mask, [1,1,3]), 'AlphaData', ~mask);
+
+    % Display an empty triangle
+    tr = Emergence_PlotTriInfo(tcn, tricol);
+    
+    % Customize the colormap
+    if iMap == 1
+        colormap(sp, [flipud(cbrewer2('YlGnBu', 2000)); ...
+                            (cbrewer2('YlOrRd', 2000))]);
+        caxis([-abs(max(caxis)), abs(max(caxis))]);
+    else, colormap(sp, LoadCWcbr); caxis([0, max(caxis)]);
+    end
+    cbr = colorbar('Location', 'SouthOutside', 'LineWidth', 1);
+    cbr.Label.String = {'Normalized density map', '(in log scale)'};
+    
+    % Draw marginal histograms on the limits of the triangle
+    if iMap > 1, Emergence_PlotMargHist(gppoints{iMap-1}, nBin); end
+    
+    % Overlap the grid corresponding to the resoution of the marginal
+    % histrograms
+    Emergence_PlotGridOnTri(nBin/2);
+    
+    % Customize the axes
+    set(gca, 'LineWidth', 1, 'FontSize', 15);
+    axis('xy'); axis('off'); axis('equal');
+end
+
+% Use the same zoom level for all the maps
+ScaleAxis('c', 1:3); ScaleAxis('x'); ScaleAxis('y');
+
+% Save the figure
+if isfield(D{1}, 'Seq'), save2pdf('figs/F_D_DensityMapsS.pdf');
+else, save2pdf('figs/F_D_DensityMapsIO.pdf');
+end
+
+%% COMPARE MARGINAL HISTOGRAMS FOR PROBABILISTIC AND DETERMINISTIC REGULARITIES
+%  ============================================================================
+
+% Create bins of beliefs
+% ~~~~~~~~~~~~~~~~~~~~~~
+
+% Sample uniformaly the probability continuum
+nBin = 51;
+pg = linspace(0, 1, nBin)';
+
+% Prepare the output variable
+bins = NaN(2,nBin,nSub);
+
+% For each type of regularity, get marginal histrograms of finger's
+% position along the relevant dimension
+for iHyp = 1:2
+    y = cellfun(@(x) histc(x(:,iHyp), pg)', subpoints(:,iHyp), 'UniformOutput', 0);
+    y = cellfun(@(x) x./sum(x), y, 'UniformOutput', 0); % normalize it
+    bins(iHyp,:,:) = cell2mat(y)';
+end
+
+% Average over subjects
+avgbins = mean(bins, 3);
+sembins = sem(bins, 3);
+
+% Compare marginal histograms
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+% Prepare a new window
+figure('Position', [802 797 200 300]); lgd = NaN(1,2);
+
+% Useful variables
+x = pg+1/(nBin-1)/2; % x-axis
+d = [1,-1]; % to plot it symmetrically
+
+% For each type of regularity, plot the histogram in a symmetrical manner
+for iHyp = 1:2
+    lgd(iHyp) = bar(x, d(iHyp).*avgbins(iHyp,:), 0.9, 'FaceColor', ...
+        tricol(iHyp,:), 'EdgeColor', 'none'); hold('on');
+    plot(repmat(x', 2, 1), d(iHyp).*avgbins(iHyp,:) + ...
+        sembins(iHyp,:).*[-1;1], 'k-', 'LineWidth', 1/2);    
+end
+
+% Overlap a line along zero 
+plot([0,1], zeros(1,2),  'k-');
+
+% Customize the axes
+SymAxis('y')
+set(gca, 'Box', 'Off', 'XLim', [0,1]);
+set(gca, 'YTick', get(gca, 'YTick'), 'YTickLabel', cellfun(@(x) ...
+    sprintf('%1.2f', x), num2cell(abs(get(gca, 'YTick'))), 'UniformOutput', 0));
+view([-90, 90]);
+
+% Display bins for which the difference is significant
+dif = squeeze(bins(1,:,:) - bins(2,:,:));
+h = ttest(dif');
+h(isnan(h)) = 0;
+plot(pg(logical(h)), max(ylim)-0.05*diff(ylim), 'kp', ...
+    'MarkerFaceColor', 'k', 'MarkerEdgeColor', 'none');
+
+% Add some text labels
+xlabel('Strength of beliefs');
+ylabel('Frequency');
+legend(lgd, cellfun(@(x) x(1:5), proclab(1:2), 'UniformOutput', 0), ...
+    'Orientation', 'Horizontal', 'Location', 'NorthOutside');
+
+% Save the figure
+if isfield(D{1}, 'Seq'), save2pdf('figs/F_D_MargHistS.pdf');
+else, save2pdf('figs/F_D_MargHistIO.pdf');
+end
---
+++ Functions/Emergence_PlotMargHist.m
@@ -0,0 +1,149 @@
+function [ marghist, bars ] = Emergence_PlotMargHist( pMgY, nBin, tricc, tricol )
+% EMERGENCE_PLOTMARGHIST draws the 3 marginal histograms along the limits
+% of the triangle. It assumes an equilateral triangle oriented with one tip
+% to the bottom and the two remaing tips to the top, like that: ^
+%   - "pMgY": a Nx3 matrix with the values of beliefs for each hypothesis.
+%   - "nBin": a scalar number specifying the number of bins.
+%   - "tricc": a 3x2 matrix specifying the x/y coordinates (columns) of
+%       each vertex of the triangle. By default the width is 1, the height
+%       sqrt(3)/2 and it is drawn at (0,0).
+%   - "tricol": a 3x3 matrix specifying the RGB (columns) of each vertex of
+%       the triangle.
+% Copyright (c) 2018 Maxime Maheu
+
+% ~~~~~~~~~~~~~~~~~~ %
+% Fill in the inputs %
+% ~~~~~~~~~~~~~~~~~~ %
+
+% Coordinates of the triangles limits (cartesian coordinates)
+if nargin < 3 || isempty(tricc)
+    tricc = [0,   sqrt(3)/2; ... % top left (P)
+             1,   sqrt(3)/2; ... % top right (D)
+             1/2, 0        ];    % bottom (R)
+end
+
+% Colors to use 
+if nargin < 4 || isempty(tricol)
+    tricol = [066 146 198; ...     % top left (blue)
+              239 059 033; ...     % top right (red)
+              065 171 093] ./ 255; % bottom (green)
+end
+
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ %
+% Get the three marginal histograms %
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ %
+
+% Get limits of the bins
+Bins = linspace(0, 1, nBin);
+
+% Compute the marginal histograms along each dimension/hypothesis
+marghist = NaN(nBin,3);
+for iDim = 1:3
+    marghist(:,iDim) = hist(pMgY(:,iDim), Bins);
+end
+
+% Normalize the histogram
+marghist = marghist./sum(marghist,1);
+
+% Prepare the variable that will entail bars' properties (colors, ...)
+bars = NaN(nBin,3);
+
+% Prepare a variable
+axlim = NaN(3,2);
+
+% For each of the 3 side of the triangle
+for iDim = 1:3
+    
+    % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ %
+    % Get the histogram for the current side of the triangle %
+    % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ %
+    
+    bins = marghist(:,iDim)';
+    if iDim == 3, bins = fliplr(bins); end
+    
+    % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ %
+    % Segment the limits of the triangle into equal parts %
+    % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ % 
+    
+    % Get x-coordinates of each segment
+    if     iDim == 1, x = linspace(tricc(1,1), tricc(3,1), nBin+1); x = fliplr(x);
+    elseif iDim == 2, x = linspace(tricc(1,1), tricc(2,1), nBin+1);
+    elseif iDim == 3, x = linspace(tricc(3,1), tricc(2,1), nBin+1);
+    end
+    
+    % Get y-coordinates of each segment
+    if iDim == 2, y = repmat(sqrt(3)/2, 1, nBin+1);
+    else,         y = linspace(0, sqrt(3)/2, nBin+1);
+    end
+    
+    % ~~~~~~~~~~~~~~~~~~~~~~~~~~~ %
+    % Get coordinates of the bars %
+    % ~~~~~~~~~~~~~~~~~~~~~~~~~~~ %
+    %  2__3      __   => Example marginal histogram with nBin = 3
+    %  |  |__   |  |  => with the location of points #1, #2, #3 and #4
+    %  |  |  |__|  |  => whose coordinates are derived below
+    % =1==4==|==|==|= => The "=" represent the limit of the triangle
+    
+    
+    % Coordinates of point #1 (on the limit of the triangle)
+    x1 = x(1:nBin);
+    y1 = y(1:nBin);
+    
+    % For the horizontal dimension (top one)
+    %   => regular (horizontal) histogram (as drawn above)
+    if iDim == 2
+        
+        % Coordinates of point #2
+        x2 = x1;
+        y2 = y1 + bins;
+        
+        % Coordinates of point #3
+        x3 = x(2:nBin+1);
+        y3 = y1 + bins;
+        
+    % For the two tilted dimensions (to the left and to the right)
+    %   => tilted histogram obtaines thanks to trigonometry
+    else
+        
+        % Coordinates of point #2
+        o = bins*sind(30); % opposite side of the right triangle
+        a = bins*cosd(30); % adjacent side of the right triangle
+        if     iDim == 1, x2 = x1-a; % to the left
+        elseif iDim == 3, x2 = x1+a; % to the right
+        end
+        y2 = y1-o; % to the top
+        
+        % Coordinates of point #3
+        o = (1/nBin)*sind(60); % opposite side of the right triangle
+        a = (1/nBin)*cosd(60); % adjacent side of the right triangle
+        if     iDim == 1, x3 = x2-a; % to the left
+        elseif iDim == 3, x3 = x2+a; % to the right
+        end
+        y3 = y2+o; % to the bottom
+    end
+    
+    % Coordinates of point #4 (on the limit of the triangle)
+    x4 = x(2:nBin+1);
+    y4 = y(2:nBin+1);
+    
+    % ~~~~~~~~~~~~~~~~~~ %
+    % Draw the histogram %
+    % ~~~~~~~~~~~~~~~~~~ %
+    
+    % Draw all the bars at the same time
+    xcoord = [x1; x2; x3; x4]; % each row is one of the vertex of the bars
+    ycoord = [y1; y2; y3; y4]; % and each column is a bin
+    bars(:,iDim) = fill(xcoord, ycoord, 'k', 'FaceColor', ...
+        tricol(iDim,:), 'LineWidth', 1); hold('on');
+    
+    % Identify x and y, min and max, limits of the histogram
+    coord = [xcoord(:); ycoord(:)];
+    axlim(iDim,:) = [min(coord), max(coord)];
+end
+
+% Tightly scale the axes such that (i) the highest bar of the histogram
+% coincides with the limit of the axes and (ii) the 
+axis(repmat([min(axlim(:,1)), max(axlim(:,2))], 1, 2));
+axis('square');
+
+end
