d6b4072ff0c2ef39b4020255c43cfc103629c32d
Maxime Maheu <maheu.mp@gmail.com>
2018-05-11 18:57:40 +0200

    IO function: observers learning probabilities

---
+++ Ideal observer/observers/Emergence_IO_Bernoulli.m
@@ -0,0 +1,233 @@
+function [ pYgTs, pAgTs, pTgY ] = Emergence_IO_Bernoulli( y, scaleme, usegrid, prior, decw, dt )
+% EMERGENCE_IO_BERNOULLI implements an observer learning the frequency of
+% items from a binary sequence.
+%   - "y": a 1xN array specifying the sequence of binary observations (1s
+%       and 2s).
+%   - "scaleme": a string ('lin' or 'log') specifying whether the model
+%       evidence sould be computed on a linear or logarithmic scale.
+%   - "usegrid": a boolean specifing whether use grid-based or analytical
+%       solutions.
+%   - "prior": can ba a string ('Bayes-Laplace' or 'Jeffreys') or a 1x2
+%       array specifying the prior knowledge regarding the frequency of
+%       transitions (expressed in pseudo-counts).
+%   - "decw": a  Nx1 or 1xN array of (decaying) weights that will weight
+%       past observations of the sequence).
+%   - "dt": a scalar specifying the grid precision for the posterior
+%       distributions over theta.
+% 
+% Copyright (c) 2018 Maxime Maheu
+
+%% Initialization
+%  ==============
+
+% Number of observations in the sequence
+K = numel(y);
+
+% By default, export the model evidence in log-scale
+if nargin < 2 || isempty(scaleme), scaleme = 'log'; end
+
+% By default, use a uniform, non-informative, prior distribution
+if nargin < 4 || isempty(prior), prior = 'Bayes-Laplace'; end
+
+% By default, use a perfect integration
+if nargin < 5 || isempty(decw), decw = ones(K,1); % no decay
+else, decw = decw(:); % make sure it is a column vector (for linear algebra below)
+end
+if numel(decw) > K, decw = decw(end-K+1:end); % take only the most recent weights
+elseif numel(decw) == K % nothing to do in that case
+else, error('Not enough weights for the length of the sequence');
+end
+
+% By default, use analytical solutions to speed up the computations
+if nargin < 3, usegrid = false; end
+
+% If the weights are uniform over the entire sequence, it means that there
+% are no memory decay, therefore analytical solutions can be used.
+% Otherwise, it means that there is a memory decay which requires to used
+% non-analytical grid-based solutions.
+if     any(decw ~= 1), usegrid = true;
+elseif all(decw == 1), usegrid = false;
+end
+
+% By default, use a small grid precision to speed-up the computation
+if nargin < 6, dt = 0.1; end
+
+% This requires to create a grid for theta
+if ~isempty(dt) && nargout > 2, returnpost = true;
+else, returnpost = false; pTgY = []; % return empty vector
+end
+
+% Create a vector for theta values, if:
+%   - we use non-analytical, grid-based, solutions, or...
+%   - we use analytical solutions but are asked to return the posterior,
+%   because in that case, the posterior is never explicitly computed. We
+%   thus have to create it afterwards, based on analytical solutions, using
+%   probability distribution functions.
+if usegrid || ~usegrid && returnpost
+    theta = 0:dt:1; % grid for theta based on grid precision
+    nt = (1/dt)+1;  % number of values for theta
+end
+
+%% Prior probabilities
+%  ===================
+
+% In case of a perfect integration, we can resort on analytical solutions
+% that require to define prior probabilities in terms of transitions'
+% pseudo-counts (and not probabilities per se) such that a conjugate prior
+% can be used.
+if ~usegrid
+    
+    % Uniform and non-informative Bayes-Laplace prior
+    if isa(prior, 'char') && strcmpi(prior, 'Bayes-Laplace')
+        p_nA = 1; % pN(A)
+        p_nB = 1; % pN(B)
+    
+	% Non-informative Jeffreys prior
+    elseif isa(prior, 'char') && strcmpi(prior, 'Jeffreys')
+        p_nA = 1/2; % pN(A)
+        p_nB = 1/2; % pN(B)
+    
+	% Custom prior
+    elseif isa(prior, 'double') && numel(prior) == 2
+        p_nA = prior(1); % pN(A)
+        p_nB = prior(2); % pN(B)
+    
+    % Catch possible mistakes
+    else, error('The prior input has the wrong form.');
+    end
+    
+% Prior beliefs over theta values
+elseif usegrid
+    
+    % Uniform and non-informative Bayes-Laplace prior
+    if isa(prior, 'char') && strcmpi(prior, 'Bayes-Laplace')
+        pT = ones(1, nt); % same value over all the grid
+
+    % Non-informative Jeffreys prior
+    elseif isa(prior, 'char') && strcmpi(prior, 'Jeffreys')
+        pT = mybetapdf(theta, 1/2, 1/2, nt);
+
+    % Custom prior
+    elseif isa(prior, 'double') && numel(prior) == 4
+        pT = mybetapdf(theta, prior(1), prior(2), nt);
+
+    % Catch possible mistakes
+    else, error('The prior input has the wrong form.');
+    end
+
+    % Make sure the prior distribution is normalized
+    pT = pT ./ sum(pT(:));
+end
+
+%% Sequence's marginal likelihood
+%  ==============================
+
+% Initialization
+% ~~~~~~~~~~~~~~
+
+% Get events' positions
+A = (y == 1);
+B = (y == 2);
+
+% Compute the model evidence in case of a perfect integration
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+% N.B. We can resort on analytical solutions
+if ~usegrid
+    
+    % Get observational counts
+    nA = sum(A); % oN(A)
+    nB = sum(B); % oN(B)
+    
+    % Combined observational and prior (pseudo-) counts
+    nX = [nA + p_nA, nB + p_nB]; % N(A) & N(B)
+    
+    % Since we use a conjugate prior, the model evidence is a beta
+    % distribution, and its integral can be analytically computed.
+    if     strcmpi(scaleme, 'lin'), pYgTs =   beta(nX(1), nX(2));
+    elseif strcmpi(scaleme, 'log'), pYgTs = betaln(nX(1), nX(2));
+    end
+    
+    % If asked, return the posterior distribution
+    if returnpost
+        pTgY = mybetapdf(theta, nX(1), nX(2), nt); % beta distribution
+        pTgY = pTgY ./ sum(pTgY(:)); % normalize the posterior
+    end
+    
+% Compute the model evidence in case of a leaky integration
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+% N.B. We need to rely on grid-based approximation solutions
+elseif usegrid
+
+    % For each item, compute the likelihood of each observation, given the
+    % weight of its corresponding position in the sequence, and for each
+    % value of theta that is considered (depending on the grid)
+    % e.g. p(observing A at position #10 with the corresponding weight
+    % of position #10 if estimated p(A) = 1/10)
+    dA = decw(A) *    theta  + (1 - decw(A)) * (1-theta);
+    dB = decw(B) * (1-theta) + (1 - decw(B)) *    theta ;
+    
+    % Compute the sequence's likelihood
+    pYgT = prod(dA, 1) .* prod(dB, 1);
+
+    % Compute the posterior distribution over theta
+    BayesNum = pYgT .* pT; % likelihood times prior (numerator in Bayes' rule)
+    pY = sum(BayesNum);    % marginal likelihood (denominator in Bayes' rule)
+    pTgY = BayesNum ./ pY; % posterior distribution over theta using Bayes' rule
+
+    % Derive (log-) model evidence
+    if     strcmpi(scaleme, 'lin'), pYgTs = pY / nt;
+    elseif strcmpi(scaleme, 'log'), pYgTs = log(pY) - log(nt);
+    end
+    % N.B. We use sum(X) / N(X) instead of the MATLAB "mean" function
+    % because it is much faster (it avoids checks that are useless in the
+    % context of this function).
+
+    % Compute the likelihood that the next observation will be a A
+    pAgTs = pTgY * theta';
+end
+
+%% Predictions
+%  ===========
+
+% Compute the likelihood that the next observation will be a A
+if    ~usegrid, pAgTs = nX(1) / sum(nX); % analytical formula (a ratio)
+elseif usegrid, pAgTs = pTgY * theta';   % based on the grid
+end
+
+%% Probability distribution
+%  ========================
+
+% Way more faster than the regular MATLAB "betapdf" function because:
+%   - it does not check the inputs, but directly "repmat" the coefficients
+%   - it does not have to check whether the inputs are smaller than 0
+%   - it does not have to check the size of x
+function  y = mybetapdf(x,a,b,n)
+
+y = zeros(1, n);
+
+a = repmat(a, [1, n]);
+b = repmat(b, [1, n]);
+
+y(a==1 & x==0) = b(a==1 & x==0);
+y(b==1 & x==1) = a(b==1 & x==1);
+y(a<1 & x==0) = Inf;
+y(b<1 & x==1) = Inf;
+
+k = a>0 & b>0 & x>0 & x<1;
+a = a(k);
+b = b(k);
+x = x(k);
+
+smallx = x<0.1;
+
+loga = (a-1).*log(x);
+
+logb = zeros(size(x), 'like', y);
+logb( smallx) = (b( smallx)-1) .* log1p(-x( smallx));
+logb(~smallx) = (b(~smallx)-1) .*  log(1-x(~smallx));
+
+y(k) = exp(loga+logb - (gammaln(a)+gammaln(b)-gammaln(a+b)));
+
+end
+
+end
---
+++ Ideal observer/observers/Emergence_IO_Markov.m
@@ -0,0 +1,308 @@
+function [ pYgTs, pAgTs, pTgY ] = Emergence_IO_Markov( y, scaleme, usegrid, prior, decw, dt )
+% EMERGENCE_IO_MARKOV implements an observer learning parameters (i.e.
+% frequency of transitions) of a first-order binary Markov chain from a
+% binary sequence.
+%   - "y": a 1xN array specifying the sequence of binary observations (1s
+%       and 2s).
+%   - "scaleme": a string ('lin' or 'log') specifying whether the model
+%       evidence sould be computed on a linear or logarithmic scale.
+%   - "usegrid": a boolean specifing whether use grid-based or analytical
+%       solutions.
+%   - "prior": can ba a string ('Bayes-Laplace' or 'Jeffreys') or a 2x2
+%       matrix specifying the prior knowledge regarding the frequency of
+%       transitions (expressed in pseudo-counts).
+%   - "decw": a  Nx1 or 1xN array of (decaying) weights that will weight
+%       past observations of the sequence).
+%   - "dt": a scalar specifying the grid precision for the posterior
+%       distributions over theta.
+% 
+% Copyright (c) 2018 Maxime Maheu
+
+%% Initialization
+%  ==============
+
+% Get the number of observations in the sequence
+K = numel(y);
+
+% By default, export the model evidence in log-scale
+if nargin < 2 || isempty(scaleme), scaleme = 'log'; end
+
+% By default, use a uniform, non-informative, prior distribution
+if nargin < 4 || isempty(prior), prior = 'Bayes-Laplace'; end
+
+% By default, use a perfect integration
+if nargin < 5 || isempty(decw), decw = ones(K,1); % no decay
+else, decw = decw(:); % make sure it is a column vector (for linear algebra below)
+end
+if numel(decw) > K, decw = decw(end-K+1:end); % take only the most recent weights
+elseif numel(decw) == K % nothing to do in that case
+else, error('Not enough weights for the length of the sequence');
+end
+
+% By default, use analytical solutions to speed up the computations
+if nargin < 3, usegrid = false; end
+
+% If the weights are uniform over the entire sequence, it means that there
+% are no memory decay, therefore analytical solutions can be used.
+% Otherwise, it means that there is a memory decay which requires to used
+% non-analytical grid-based solutions.
+if     any(decw ~= 1), usegrid = true;
+elseif all(decw == 1), usegrid = false;
+end
+
+% By default, use a small grid precision to speed-up the computation
+if nargin < 6, dt = 0.1; end
+
+% This requires to create a grid for theta
+if ~isempty(dt) && nargout > 2, returnpost = true;
+else, returnpost = false; pTgY = []; % return empty vector
+end
+
+% Create a vector for theta values, if:
+%   - we use non-analytical, grid-based, solutions, or...
+%   - we use analytical solutions but are asked to return the posterior,
+%   because in that case, the posterior is never explicitly computed. We
+%   thus have to create it afterwards, based on analytical solutions, using
+%   probability distribution functions.
+if usegrid || ~usegrid && returnpost
+    theta = 0:dt:1; % grid for theta based on grid precision
+    nt = (1/dt)+1;  % number of values for theta
+end
+
+%% Prior probabilities
+%  ===================
+
+% In case of a perfect integration, we can resort on analytical solutions
+% that require to define prior probabilities in terms of transitions'
+% pseudo-counts (and not probabilities per se) such that a conjugate prior
+% can be used.
+if ~usegrid
+    
+    % Uniform and non-informative Bayes-Laplace prior
+    if isa(prior, 'char') && strcmpi(prior, 'Bayes-Laplace')
+        p_nAgB = 1; % pN(A|B)
+        p_nBgA = 1; % pN(B|A)
+        p_nAgA = 1; % pN(A|A)
+        p_nBgB = 1; % pN(B|B)
+        
+    % Non-informative Jeffreys prior
+    elseif isa(prior, 'char') && strcmpi(prior, 'Jeffreys')
+        p_nAgB = 1/2; % pN(A|B)
+        p_nBgA = 1/2; % pN(B|A)
+        p_nAgA = 1/2; % pN(A|A)
+        p_nBgB = 1/2; % pN(B|B)
+        
+    % Custom prior
+    elseif isa(prior, 'double') && numel(prior) == 4
+        p_nAgB = prior(2,1); % pN(A|B)
+        p_nBgA = prior(1,2); % pN(B|A)
+        p_nAgA = prior(1,1); % pN(A|A)
+        p_nBgB = prior(2,2); % pN(B|B)
+        
+    % Catch possible mistakes
+    else, error('The prior input has the wrong form.');
+    end
+    
+% Prior beliefs over theta values    
+elseif usegrid
+    
+    % Uniform and non-informative Bayes-Laplace prior
+    if isa(prior, 'char') && strcmpi(prior, 'Bayes-Laplace')
+        pT = ones(nt, nt); % same value over all the grid
+        
+    % Non-informative Jeffreys prior
+    elseif isa(prior, 'char') && strcmpi(prior, 'Jeffreys')
+        pTgA = mybetapdf(theta, 1/2, 1/2, nt); % p(X|A), x-axis
+        pTgB = mybetapdf(theta, 1/2, 1/2, nt); % p(X|B), y-axis
+        pT = pTgA'*pTgB;
+        
+    % Custom prior
+    elseif isa(prior, 'double') && numel(prior) == 4
+        pTgA = mybetapdf(theta, prior(1,2), prior(1,1), nt); % p(X|A), x-axis
+        pTgB = mybetapdf(theta, prior(2,1), prior(2,2), nt); % p(X|B), y-axis
+        pT = pTgA'*pTgB;
+        
+    % Catch possible mistakes
+    else, error('The prior input has the wrong form.');
+    end
+    
+    % Make sure the prior distribution is normalized
+    pT = pT ./ sum(pT(:));
+end
+
+%% Sequence's marginal likelihood
+%  ==============================
+
+% Initialization
+% ~~~~~~~~~~~~~~
+
+% The likelihood of the first event is simply 1 over the number of
+% different possible stimuli in the sequence.
+% p(y_1) = 1/2 <=> log(p(y_1)) = -log(2)
+pY1 = 1/2;
+
+% Get the identity of the previous observations of each observation in the
+% sequence. This eases the frequency counts of transitions.
+cond = cat(2, NaN, y(1:end-1));
+
+% Get events' positions
+A = (y == 1); % observations that are A
+B = (y == 2); % observations that are B
+gA = cond(A); % observations that follow an A
+gB = cond(B); % observations that follow a B
+
+% Get positions of each transition in the sequence
+AgA = false(1, K); AgB = AgA; BgA = AgA; BgB = AgA;
+AgA(A) = (gA == 1); % A => A cases
+BgA(B) = (gB == 1); % A => B cases
+AgB(A) = (gA == 2); % A => B cases
+BgB(B) = (gB == 2); % B => B cases
+
+% Get observational counts
+nAgA = sum(AgA);
+nAgB = sum(AgB);
+nBgA = sum(BgA);
+nBgB = sum(BgB);
+
+% Compute the model evidence in case of a perfect integration
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+% N.B. We can resort on analytical solutions
+if ~usegrid
+    
+    % Combined observational and prior (pseudo-) counts.
+    nXgA = [nAgA + p_nAgA, nBgA + p_nBgA]; % N(A|A) + pN(A|A) & N(B|A) + pN(B|A)
+    nXgB = [nAgB + p_nAgB, nBgB + p_nBgB]; % N(A|B) + pN(A|B) & N(B|B) + pN(B|B)
+    
+    % Get the number of observations that followed an A versus a B
+    ngA = sum(nXgA);
+    ngB = sum(nXgB);
+    
+    % The integral of a beta distribution with parameters that are
+    % (pseudo-) transitions' counts can be solved analytically by the
+    % means of gamma distributions
+    % p(y|Msp) = p(y(1)) int[p(y_2:K|t(A|B),t(B|A))] dt
+    if strcmpi(scaleme, 'lin')
+        pYp2gTgA = prod(gamma(nXgA)) ./ gamma(ngA);
+        pYp2gTgB = prod(gamma(nXgB)) ./ gamma(ngB);
+    elseif strcmpi(scaleme, 'log')
+        pYp2gTgA = sum(gammaln(nXgA)) - gammaln(ngA);
+        pYp2gTgB = sum(gammaln(nXgB)) - gammaln(ngB);
+    end
+    
+    % The likelihood is the product of the two integrals and the
+    % likelihood of the first event:
+    % p(y|t(A|B),t(B|A)) = p(y2(1)) * p(y|t(X|A)) * p(y2|t(X|B))
+    % <=> log(p(y2|t(A|B),t(B|A))) = log(p(y2(1))) + log(p(y2|t(X|A))) + log(p(y2|t(X|B)))
+    if     strcmpi(scaleme, 'lin'), pYgTs =     pY1  * pYp2gTgA * pYp2gTgB;
+    elseif strcmpi(scaleme, 'log'), pYgTs = log(pY1) + pYp2gTgA + pYp2gTgB;
+    end
+    
+    % If asked, return the posterior distribution
+    if returnpost
+        pTgA = mybetapdf(theta, nXgA(1,2), nXgA(1,1), nt); % p(X|A), x-axis
+        pTgB = mybetapdf(theta, nXgB(1,1), nXgB(1,2), nt); % p(X|B), y-axis
+        pTgY = pTgA' * pTgB; % create the 2D probability distribution
+        pTgY = pTgY ./ sum(pTgY(:)); % normalize the posterior
+    end
+    
+    % Compute the expected value of each transition
+    pXgA = nXgA ./ ngA; % p(A|A) & p(B|A)
+    pXgB = nXgB ./ ngB; % p(A|B) & p(B|B)
+
+% Compute the model evidence in case of a leaky integration
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+% N.B. We need to rely on grid-based approximation solutions
+elseif usegrid
+    
+    % Get the weights corresponding to the positions at which each
+    % transition was observed
+    wAgA = decw(AgA); if isempty(wAgA), wAgA = 1/2; end
+    wBgA = decw(BgA); if isempty(wBgA), wBgA = 1/2; end
+    wAgB = decw(AgB); if isempty(wAgB), wAgB = 1/2; end
+    wBgB = decw(BgB); if isempty(wBgB), wBgB = 1/2; end
+    
+    % For each transition, compute the likelihood of each observation,
+    % given the weight of its corresponding position in the sequence, and
+    % for each value of theta that is considered (depending on the grid)
+    % e.g. p(observing A|A at position #10 with the corresponding weight
+    % of position #10 if estimated p(A|A) = 1/10)
+    dAgA = wAgA * (1-theta) + (1 - wAgA) *    theta ;
+    dBgA = wBgA *    theta  + (1 - wBgA) * (1-theta);
+    dAgB = wAgB *    theta  + (1 - wAgB) * (1-theta);
+    dBgB = wBgB * (1-theta) + (1 - wBgB) *    theta ;
+	
+    % Compute the sequence's likelihood by combining different observations
+    % of transitions together
+    pYgtBgA = prod(dAgA, 1) .* prod(dBgA, 1); % marginal distribution
+    pYgtAgB = prod(dAgB, 1) .* prod(dBgB, 1); % marginal distribution
+    pYgT = pY1 .* pYgtBgA' .* pYgtAgB;        % joint distribution
+    
+    % Compute the posterior distribution over theta
+    BayesNum = pYgT .* pT; % likelihood times prior (numerator in Bayes' rule)
+    pY = sum(BayesNum(:)); % marginal likelihood (denominator in Bayes' rule)
+    pTgY = BayesNum ./ pY; % posterior distribution over theta using Bayes' rule
+    
+    % Derive (log-) model evidence
+    if     strcmpi(scaleme, 'lin'), pYgTs = pY / (nt^2);
+    elseif strcmpi(scaleme, 'log'), pYgTs = log(pY) - 2*log(nt);
+    end
+    % N.B. We use sum(X) / N(X) instead of the MATLAB "mean" function
+    % because it is much faster (it avoids checks that are useless in the
+    % context of this function).
+    
+	% Compute the expected value of each transition
+    pTgA = sum(pTgY, 2)'; % marginal distribution (vector over theta values)
+    pTgB = sum(pTgY, 1) ; % marginal distribution (vector over theta values)
+    pBgA = (pTgA / sum(pTgA)) * theta'; % expected value of theta(X|A)
+    pAgB = (pTgB / sum(pTgB)) * theta'; % expected value of theta(X|B)
+    pXgA = [1 - pBgA, pBgA]; % p(A|A) & p(B|A)
+    pXgB = [pAgB, 1 - pAgB]; % p(A|B) & p(B|B)
+end
+
+%% Predictions
+%  ===========
+
+% Compute the likelihood that the next observation will be a A (an
+% analytical formula can be used, which is simply a ratio).
+% N.B. This depends on the identity of the previously received observation.
+pAgX  = [pXgA(1), pXgB(1)]; % p(A|A) & p(A|B)
+X     = y(end);  % get the identity of the last observation
+pAgTs = pAgX(X); % returns p(A) conditionaly on the last observation
+
+%% Probability distribution
+%  ========================
+
+% Way more faster than the regular MATLAB "betapdf" function because:
+%   - it does not check the inputs, but directly "repmat" the coefficients
+%   - it does not have to check whether the inputs are smaller than 0
+%   - it does not have to check the size of x
+function  y = mybetapdf(x,a,b,n)
+
+y = zeros(1, n);
+
+a = repmat(a, [1, n]);
+b = repmat(b, [1, n]);
+
+y(a==1 & x==0) = b(a==1 & x==0);
+y(b==1 & x==1) = a(b==1 & x==1);
+y(a<1 & x==0) = Inf;
+y(b<1 & x==1) = Inf;
+
+k = a>0 & b>0 & x>0 & x<1;
+a = a(k);
+b = b(k);
+x = x(k);
+
+smallx = x<0.1;
+
+loga = (a-1).*log(x);
+
+logb = zeros(size(x), 'like', y);
+logb( smallx) = (b( smallx)-1) .* log1p(-x( smallx));
+logb(~smallx) = (b(~smallx)-1) .*  log(1-x(~smallx));
+
+y(k) = exp(loga+logb - (gammaln(a)+gammaln(b)-gammaln(a+b)));
+
+end
+
+end
