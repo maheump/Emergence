aecdb8830e760bea2b3c9a7bd499c2cb5ce763e5
Maxime Maheu <maheu.mp@gmail.com>
2018-08-28 21:51:25 +0200

    !!! Add a SETUP script !!!

---
+++ SETUP.m
@@ -0,0 +1,26 @@
+% Setup default options and add required functions and scripts to the
+% MATLAB path.
+%
+% Copyright (c) 2018 Maxime Maheu
+
+% Clear the place
+clear;
+close('all');
+
+% Locate
+scriptpath = mfilename('fullpath');
+folderpath = scriptpath(1:strfind(scriptpath,'Emergence')+8);
+
+% Unzip folder containing various general purpose functions
+unzip(fullfile(folderpath, 'Functions', 'dependencies.zip'));
+
+% Add scripts and functions to the MATLAB path
+addpath(genpath(folderpath));
+
+% Add the VBA toolbox to the path
+try VBA_Setup;
+catch warning('The VBA toolbox is not available. Some scripts will not run');
+end
+
+% Set default properties for the figures
+Emergence_DefaultFigureProperties;
