a526e4ce97a815156d9c5bf9778b290b37a41ab9
Maxime Maheu <maheu.mp@gmail.com>
2018-08-29 11:20:54 +0200

    Unzip in the right directory

--- SETUP.m
+++ SETUP.m
@@ -12,14 +12,17 @@ scriptpath = mfilename('fullpath');
 folderpath = scriptpath(1:strfind(scriptpath,'Emergence')+8);
 
 % Unzip folder containing various general purpose functions
-unzip(fullfile(folderpath, 'Functions', 'dependencies.zip'));
+unzip(fullfile(folderpath, 'Functions', 'dependencies.zip'), ...
+      fullfile(folderpath, 'Functions'));
 
 % Add scripts and functions to the MATLAB path
 addpath(genpath(folderpath));
 
 % Add the VBA toolbox to the path
-try VBA_Setup;
-catch warning('The VBA toolbox is not available. Some scripts will not run');
+try
+    VBA_Setup;
+catch
+    warning('The VBA toolbox is not available. Some scripts will not run');
 end
 
 % Set default properties for the figures
