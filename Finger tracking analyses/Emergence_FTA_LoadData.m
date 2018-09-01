6ffacabdca58b5dbf582f54a58819853b1dea0ff
Maxime Maheu <maheu.mp@gmail.com>
2018-09-01 20:21:08 +0200

    Add verbose to the script that loads data

--- Finger tracking analyses/Emergence_FTA_LoadData.m
+++ Finger tracking analyses/Emergence_FTA_LoadData.m
@@ -1,15 +1,15 @@
 % This script setups the MATLAB environment and figures' preferences
 % required to run Emergence_FTA_* analysis scripts.
-% 
+%
 % Copyright (c) 2018 Maxime Maheu
+
+% Display info
+fprintf('Loading data... ');
 
 % Add functions to the MATLAB path
 scriptpath = mfilename('fullpath');
 folderpath = scriptpath(1:strfind(scriptpath,'Emergence')+8);
 addpath(genpath(folderpath));
-
-% Set default figure properties
-Emergence_DefaultFigureProperties;
 
 % Load the data
 load('Emergence_Behaviour_GroupData.mat');
@@ -23,6 +23,8 @@ if ~exist('rmbadsub', 'var') || rmbadsub
     G = G(:,goodsubs);
     S = S(goodsubs);
     IO = IO(:,goodsubs);
+    rmbadsub = true;
+else, rmbadsub = false;
 end
 
 % Get the number of subjects and the number of conditions
@@ -33,3 +35,9 @@ D = G;
 
 % Get the length of each sequence
 N = numel(G{1}.Seq);
+
+% Display info
+fprintf('Done! ');
+if      rmbadsub, fprintf('Bad subjects are excluded.\n');
+elseif ~rmbadsub, fprintf('Bad subjects are NOT excluded.\n');
+end
