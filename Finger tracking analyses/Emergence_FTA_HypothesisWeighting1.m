6ede30a073d7be8a22342fe9eba3d69179ce86cd
Maxime Maheu <maheu.mp@gmail.com>
2018-11-21 16:09:24 +0100

    Add P/D ratio + change window + change colorbar

--- Finger tracking analyses/Emergence_FTA_HypothesisWeighting1.m
+++ Finger tracking analyses/Emergence_FTA_HypothesisWeighting1.m
@@ -15,45 +15,6 @@
 %% SHOW THAT DETERMINISTIC REGULARITIES ARE (ALSO) CHARACTERIZED BY DIFFERENT ENTROPY LEVELS
 %  =========================================================================================
 
-% Align observers' trajectories in sequences with deterministic
-% regularities to the detection points
-% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-% Theshold in beliefs that define the detection points
-detecthr = 1/2;
-
-% Window in which to look in (in # of observations)
-winwidth = 80;
-xval = -winwidth/2:winwidth/2;
-
-% Get the position of the change point
-cp = cellfun(@(xval) xval.Jump-1/2, G(cidx{2},:));
-
-% Get the position of the detection point
-cdp = cellfun(@(xval,c) c + find(xval.BarycCoord(c:end,2) > detecthr, 1, ...
-    'first') - 1, D(cidx{2},:), num2cell(cp), 'UniformOutput', 0); % cells
-
-% Remove sequences with deterministic regularities that were not detected
-detecmask = cellfun(@(xval) xval.Questions(2) == 2, G(cidx{2},:));
-cdp(~detecmask) = {[]};
-
-% Beginning and ending of the window to look into
-idx = cellfun(@(xval) xval-winwidth/2:xval+winwidth/2, cdp, 'UniformOutput', 0);
-
-% Create a useful logical indexing that makes sure that the window around
-% the detection point does not go beyond the observations' indices of the
-% sequence
-ok = cellfun(@(xval) xval >= 1 & xval <= N, idx, 'UniformOutput', 0);
-
-% Get the posterior beliefs over the 3 different hypotheses
-beliefs = cellfun(@(xval) xval.BarycCoord, D(cidx{2},:), 'UniformOutput', 0);
-
-% Select posterior beliefs around the detection point
-subtraj = cellfun(@(xval) NaN(winwidth+1,3), cell(size(idx)), 'UniformOutput', 0);
-for iSeq = 1:numel(cdp)
-    subtraj{iSeq}(ok{iSeq},:) = beliefs{iSeq}(idx{iSeq}(ok{iSeq}),:);
-end
-
 % Compute entropy of the transition probabilities from the rules
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -83,8 +44,10 @@ for iEnt = 1:nEnt
 end
 
 % Get colormap for the entropy
-EntGrid = linspace(0, 1, 1001);
-EntCMap = hot(numel(EntGrid));
+EntGrid = linspace(0, 1, 111);
+EntCMap = flipud(cbrewer2('RdPu', numel(EntGrid)));
+EntCMap = EntCMap(1:101,:);
+EntGrid = linspace(0, 1, 101);
 
 % Compute 2D entropy map
 EntMap = arrayfun(@Emergence_IO_Entropy, EntGrid);
@@ -159,20 +122,137 @@ xlabel('p(A|B)'); ylabel('p(B|A)');
 % Save the figure
 save2pdf(fullfile(ftapath, 'figs', 'F_HW_DeterRegTP.pdf'));
 
-%% SHOW THAT THE TRANSIENT BELIEFS DEPENDS UPON THE ENTROPY OF THE RULES
+%% SHOW THE TRANSIENT TENSION BETWEEN REGULAR HYPOTHESES
+%  =====================================================
+
+% Align observers' trajectories in sequences with deterministic
+% regularities to the detection points
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+% Theshold in beliefs that define the detection points
+detecthr = 1/2;
+
+% Window in which to look in (in # of observations)
+xval = -130:60;
+nsp = numel(xval);
+
+% Get the detection point's position
+% (note that we go back in time from the end of the sequence in order
+% to avoid as much as possible the false alarms that might exist at the
+% time of the change point)
+cdp = cellfun(@(x) min([NaN, ...
+    N - find(flipud(x.BarycCoord( x.Jump + 1/2: end, 2)) ...
+    >= detecthr, 1, 'last')], [], 'OmitNaN'), ... % in # of observations
+    D(cidx{2},:), 'UniformOutput', 0); % cells
+    
+% Only detected regularities
+detecmask = cellfun(@(x) x.Questions(2) == 2, G(cidx{2},:));
+cdp(~detecmask) = {NaN};
+
+% Beginning and ending of the window to look into
+idx = cellfun(@(x) x+xval, cdp, 'UniformOutput', 0);
+
+% Create a useful logical indexing that makes sure that the window around
+% the detection point does not go beyond the observations' indices of the
+% sequence
+ok = cellfun(@(x) x >= 1 & x <= N, idx, 'UniformOutput', 0);
+
+% Get the posterior beliefs over the 3 different hypotheses
+beliefs = cellfun(@(x) x.BarycCoord, D(cidx{2},:), 'UniformOutput', 0);
+
+% Select posterior beliefs around the detection point
+subtraj = cellfun(@(x) NaN(nsp,3), cell(size(idx)), 'UniformOutput', 0);
+for iSeq = 1:numel(idx)
+    subtraj{iSeq}(ok{iSeq},:) = beliefs{iSeq}(idx{iSeq}(ok{iSeq}),:);
+end
+
+% Average trajectories over sequences and subjects
+avgtraj = squeeze(mean(cell2mat(cellfun(@(x) reshape(x, [1 1 size(x)]), ...
+    subtraj, 'UniformOutput', 0)), [1,2], 'OmitNaN'));
+cc = avgtraj*tricc;
+
+% Show the trajectory in the triangle locked on the detection of
+% deterministic regularities
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+% For each predictor
+figure('Position', [292 905 300 200]);
+
+% Display information about the triangle
+Emergence_PlotTriInfo; hold('on');
+l = Emergence_PlotGridOnTri(2, 2); set(l(1,2), 'LineWidth', 2);
+
+% Display null ratio
+plot(ones(1,2)./2, [0, sqrt(3)/2], 'k--');
+
+% Display the average trajectory
+plot(cc(:,1), cc(:,2), 'k-', 'LineWidth', 2);
+
+% Customize the axes
+axis([0, 1, 0, sqrt(3)/2]);
+axis('xy'); axis('off'); axis('equal');
+
+% Save the figure
+if isfield(D{1}, 'Seq'), save2pdf(fullfile(ftapath, 'figs', 'F_HW_Tri.pdf'));
+else, save2pdf(fullfile(ftapath, 'figs', 'F_HW_Tri.pdf'));
+end
+
+% Display the P/D ratio locked on the detection of deterministic regularities
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+% Copute the P/D ratio
+ratioPD = cell2mat(cellfun(@(x) reshape(x(:,1) ./ sum(x(:,1:2), 2), ...
+    [1 1 nsp]), subtraj, 'UniformOutput', 0));
+
+% Transform it such that it evolves between -1 (Deter) and 1 (Proba)
+ratioPD = 2 .* (ratioPD - 1/2);
+
+% Average over sequences for each subject
+ratioPD = squeeze(mean(ratioPD, 1, 'OmitNaN'));
+
+% Average over subjects
+avg = mean(ratioPD, 1);
+err = sem( ratioPD, 1);
+
+% Create a new window
+figure('Position', [593 905 190 200]);
+
+% Display origin
+plot(xval([1,end]), zeros(1,2), 'k-'); hold('on');
+plot(zeros(1,2), [-1,1], 'k-');
+
+% Display the P/D ratio centered on the detection point
+plotMSEM(xval, avg, err, 1/5, 'k', 'k', 2);
+
+% Customize the axes
+caxis([-1,1]);
+axis([xval(1), xval(end), -1, 0.5]);
+set(gca, 'Box', 'Off');
+
+% Add text labels
+xlabel('Observation w.r.t. detection point');
+ylabel('Ratio P versus D');
+
+% Save the figure
+if isfield(D{1}, 'Seq'), save2pdf(fullfile(ftapath, 'figs', 'F_HW_PDratio.pdf'));
+else, save2pdf(fullfile(ftapath, 'figs', 'F_HW_PDratio.pdf'));
+end
+
+%% SHOW THAT THE TRANSIENT TENSION DEPENDS UPON THE ENTROPY OF THE RULES
 %  =====================================================================
 
 % Average trajectories around detection point for each entropy bin
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 % Prepare an output variable
-subavgtrajent = NaN(winwidth+1,3,nEnt,nSub);
+subavgtrajent = NaN(nsp,3,nEnt,nSub);
 
 % For each entropy bin, average the trajectories across subjects
 for iEnt = 1:nEnt
     n = numel(EntIdx{iEnt});
     d = mat2cell(subtraj(EntIdx{iEnt},:), n, ones(nSub,1));
-    d = cellfun(@(xval) mean(cell2mat(reshape(xval, [1,1,n])), 3, 'OmitNaN'), d, 'UniformOutput', 0);
+    d = cellfun(@(x) mean(cell2mat(reshape(x, [1,1,n])), 3, 'OmitNaN'), ...
+        d, 'UniformOutput', 0);
     subavgtrajent(:,:,iEnt,:) = cell2mat(reshape(d, [1,1,nSub]));
 end
 
@@ -184,7 +264,7 @@ gdsemtrajent = sem(subavgtrajent, 4);
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 % Prepare a new window
-figure('Position', [292 905 240 200]);
+figure('Position', [784 905 240 200]);
 
 % Display the triangle
 Emergence_PlotTriInfo(tricc, tricol);
@@ -203,7 +283,7 @@ end
 
 % Display relevant triangular grid
 Emergence_PlotGridOnTri(10, 1, tricol(1,:), tricc);
-Emergence_PlotGridOnTri(2,  2, tricol(2,:), tricc);
+l = Emergence_PlotGridOnTri(2, 2); set(l(1,2), 'LineWidth', 2);
 
 % Make sure the triangle is equilateral and the axes invisible
 axis('equal'); axis('off');
@@ -217,19 +297,19 @@ end
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 % Prepare a new window
-figure('Position', [533 905 240 200]);
+figure('Position', [1025 905 240 200]);
 
 % Draw some help lines
-plot(xval([1,end]),    ones(1,2)./2, '-',  'Color', g, 'LineWidth', 1/2); hold('on');
-plot(xval([1,end]),    ones(1,2)./3, '--', 'Color', g, 'LineWidth', 1/2);
-plot(xval([1,end]), 2.*ones(1,2)./3, '--', 'Color', g, 'LineWidth', 1/2);
+plot(xval([1,end]), ones(1,2)./2, '-',  'Color', g, 'LineWidth', 1/2); hold('on');
 plot(zeros(1,2), [0,1], 'k');
 
 % For each entropy bin
 for iEnt = 1:nEnt
     col = EntCMap(colidx(iEnt),:);
-    plotMSEM(xval, gdavgtrajent(:,1,iEnt), gdsemtrajent(:,1,iEnt), 1/10, col, col, 3, 1, '-', 'none');
-    plotMSEM(xval, gdavgtrajent(:,2,iEnt), gdsemtrajent(:,1,iEnt), 1/10, col, col, 1, 1, '--', 'none');
+    plotMSEM(xval, gdavgtrajent(:,1,iEnt), gdsemtrajent(:,1,iEnt), ...
+        1/10, col, col, 3, 1, '-', 'none');
+    plotMSEM(xval, gdavgtrajent(:,2,iEnt), gdsemtrajent(:,1,iEnt), ...
+        1/10, col, col, 1, 1, '--', 'none');
 end 
 
 % Customize the axes
@@ -251,8 +331,9 @@ end
 
 % Get the average beliefs in the probabilistic hypothesis between the
 % change and the detection points
-transpbel = cellfun(@(b,c,d) mean(b.BarycCoord(c:d,1)), ...
-    D(cidx{2},:), num2cell(cp), cdp);
+cdp(cellfun(@isnan, cdp)) = {[]};
+transpbel = cellfun(@(x,d) mean(x.BarycCoord((x.Jump+1/2):d,1)), ...
+    D(cidx{2},:), cdp);
 
 % Remove sequences with deterministic regularities that were not detected
 transpbel(~detecmask) = NaN;
@@ -271,7 +352,7 @@ disp(RMtbl);
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 % Prepare a new window
-figure('Position', [774 905 120 200]);
+figure('Position', [1266 905 120 200]);
 
 % Display the dispersion of of beliefs across subjects
 Emergence_PlotSubGp(data, EntCMap(colidx,:));
