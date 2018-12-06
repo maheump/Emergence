8b92c52c684237a0328402599210ca8062b94005
Maxime Maheu <maheu.mp@gmail.com>
2018-12-06 18:17:26 +0100

    Major update:  "*_IO_InvestigateTreeDepth"
    Mirror properties of AlternativeDeterHypothesis to InvestigateTreeDepth
    Add option to restrict analysis to correctly labeled sequences
    Generalize analysis to all sequences (not only deterministic)

--- Ideal observer/Simulations/Emergence_IO_InvestigateTreeDepth.m
+++ Ideal observer/Simulations/Emergence_IO_InvestigateTreeDepth.m
@@ -1,15 +1,14 @@
 % This simulation script compares the posterior beliefs of the
 % deterministic Bayesian observer learning repeating rules using different
 % tree depth (i.e. the longest possible rule that is considered).
-%
+% 
 % Copyright (c) 2018 Maxime Maheu
 
 %% INITIALIZATION
 %  ==============
 
 % Clear the place
-clear;
-close('all');
+clear; close('all');
 
 % Add ideal observer functions to the MATLAB path
 scriptpath = mfilename('fullpath');
@@ -20,44 +19,9 @@ addpath(genpath(folderpath));
 % Set default figure properties
 Emergence_DefaultFigureProperties;
 
-%% DEFINE SEQUENCES' PROPERTIES
-%  ============================
-
-% Whether to run simulations on simulated sequences (opt = 1) or on
-% sequences that were presented to subjects (opt = 2)
-opt = 2;
-
-% Newly simulated sequences
-% ~~~~~~~~~~~~~~~~~~~~~~~~~
-if opt == 1
-    
-    % Define the rules to use in the simulations
-    dr = {'AABB', ...
-          'AAABBB', 'AABABB', 'AAABAB'...
-          'AAAABBBB', 'AABABABB', 'AAABBABB', ...
-          'AAAAABBBBB', 'AABABABABB', 'AAABAABBAB'};
-    
-    % Define the number of sequences to simulate for each rule
-    % N.B. The results can be interpreted as soon as with nSeq = 2 but the
-    % asymptotical results are reached for nSeq = 50
-    nSub = 50;
-    
-    % Define the position of the change point (it is always the same)
-    cp = 100;
-    
-    % Define the length of the sequences
-    N = 200;
-
-% Sequences presented to subjects
-% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-elseif opt == 2
-    
-    % Load data
-    Emergence_FTA_LoadData;
-end
-
-% Get the number of deterministic regularities
-nReg = numel(dr);
+% Get sequences presented to subjects
+Emergence_FTA_LoadData;
+nSeq = size(G,1);
 
 %% DEFINE PROPERTIES OF THE IDEAL OBSERVER
 %  =======================================
@@ -67,143 +31,164 @@ Nu = [unique(cellfun(@numel, dr)') 20 50 N];
 nNu = numel(Nu);
 
 % Define options of the ideal observer
-pEd   = 0; % probability of making a memory error at each observation
-pEp   = 0; % probability of making a memory error at each observation
-treed = 10; % depth of the rules' tree to explore
-stat  = 'Transitions'; % statistic to be learned by the probabilistic model
-p_pR  = 'Size-principle'; % the prior probability of each rule depends on its length
-p_pT  = 'Bayes-Laplace'; % the prior over statistics to be learnt
-p_pJ  = 'Uniform'; % prior over change point's position
-comp  = 'all'; % compute after each observation
-scale = 'log'; % scale of the model evidence
-verb  = 0; % do not output messages in the command window
-pgrid = []; % do not ask for the posterior distributions
+pEd   = 0;                  % probability of making a memory error at each observation
+pEp   = 0;                  % probability of making a memory error at each observation
+treed = 10;                 % depth of the rules' tree to explore
+stat  = 'Transitions';      % statistic to be learned by the probabilistic model
+p_pR  = 'Size-principle';   % the prior probability of each rule depends on its length
+p_pT  = 'Bayes-Laplace';    % the prior over statistics to be learnt
+p_pJ  = 'Uniform';          % prior over change point's position
+comp  = 'all';              % compute after each observation
+scale = 'log';              % scale of the model evidence
+verb  = 0;                  % do not output messages in the command window
+pgrid = [];                 % do not ask for the posterior distributions
 
 %% SIMULATION LOOP
 %  ===============
 
 % Prepare the output variable
-pMdgY = NaN(N,nSub,nReg,nNu);
+pMdgY = NaN(N,nSub,nSeq,nNu);
 
-% For each repeating rule
-for iReg = 1:nReg
+% For each sequence
+for iSeq = 1:nSeq
     
-    % For each simulated sequence
+    % For each subject
     for iSub = 1:nSub
-        
-        % Get the sequence
-        if opt == 1 % generate
-            nrepet = ceil((N-cp)/numel(dr{iReg}));
-            seq = [GenRandSeq(cp, 1/2), ...                 % first part
-                   repmat(str2pat(dr{iReg}), [1,nrepet])];  % second part
-        elseif opt == 2 % retrieve
-            seq = G{cidx{2}(iReg),iSub}.Seq;
-        end
         
         % For each depth of the tree
         for iNu = 1:nNu
             
             % Display the status of the loop in the command window
-            fprintf(['- Rule %2.0f/%2.0f (%s), Sequence %2.0f/%2.0f, ', ...
+            fprintf(['- Rule %2.0f/%2.0f, Sequence %2.0f/%2.0f, ', ...
                 'Depth %1.0f/%1.0f (nu = %3.0f)... '], ...
-                iReg, nReg, dr{iReg}, iSub, nSub, iNu, nNu, Nu(iNu));
+                iSeq, nSeq, iSub, nSub, iNu, nNu, Nu(iNu));
             
             % Run the ideal observer
-            io = Emergence_IO_FullIO(seq(1:N), pEd, pEp, Nu(iNu), ...
-                stat, p_pR, p_pT, p_pJ, comp, scale, pgrid, verb);
-            pMdgY(:,iSub,iReg,iNu) = io.pMsdgY;
+            io = Emergence_IO_FullIO(G{iSeq,iSub}.Seq, pEd, pEp, ...
+                Nu(iNu), stat, p_pR, p_pT, p_pJ, comp, scale, pgrid, verb);
+            pMdgY(:,iSub,iSeq,iNu) = io.pMsdgY;
         end
+        
+        % Save temporary file
+        save('Emergence_IO_InvestigateTreeDepth_tmp.mat', 'pMdgY');
     end
 end
 
-% Save simulations
+% Save file containing all the simulations and delete temporary file
 save('Emergence_IO_InvestigateTreeDepth.mat', 'pMdgY');
+delete('Emergence_IO_InvestigateTreeDepth_tmp.mat');
 
-%% COMPUTE CONFUSION MATRIX
-%  ========================
+%% COMPUTE SIMILARITY MATRICES
+%  ===========================
+
+% Whether to restrict the analysis to sequences that were correctly
+% identified by subjects (allows comparison with other analyses in which
+% data from only accurately detected regularities are shown)
+RestToIdent = true;
+
+% Get correctly labeled sequences
+TrueSeqType = cellfun(@(x) find(strcmpi(x.Cond(1), {'P', 'D', 'S'})), G);
+ReportedSeqType = cellfun(@(x) x.Questions(2), G);
+ReportedSeqType(isnan(ReportedSeqType)) = 3;
+CorrectIdent = TrueSeqType == ReportedSeqType;
 
 % Prepare the output variables
-CorMat = NaN(nNu,nNu,nSub,nReg);
+CorMat = NaN(nNu,nNu,nSub,nSeq);
 MseMat = CorMat;
 
-% For each repeating rule
-for iReg = 1:nReg
+% For each sequence
+for iSeq = 1:nSeq
     
-    % For each simulated sequence
+    % For each subject
     for iSub = 1:nSub
-    
-        % For each pair of tree depth
-        for iNu1 = 1:nNu
-            for iNu2 = 1:nNu
-                
-                % Get posterior beliefs of observers using different tree depth
-                x = pMdgY(2:end,iSub,iReg,iNu1);
-                y = pMdgY(2:end,iSub,iReg,iNu2);
-                
-                % Measure the correlation between the two
-                CorMat(iNu1,iNu2,iSub,iReg) = corr(x, y);
-                
-                % Measure the mean squared difference between the two
-                MseMat(iNu1,iNu2,iSub,iReg) = -log(mean((x - y) .^ 2));
+        
+        % Remove data from sequences that were not correctly labeled by
+        % human subjects
+        if ~RestToIdent || RestToIdent && CorrectIdent(iSeq,iSub)
+            
+            % For each pair of tree depth
+            for iNu1 = 1:nNu
+                for iNu2 = 1:nNu
+                    
+                    % Get posterior beliefs of observers using different tree depth
+                    x = pMdgY(2:end,iSub,iSeq,iNu1);
+                    y = pMdgY(2:end,iSub,iSeq,iNu2);
+                    
+                    % Measure the correlation between the two
+                    CorMat(iNu1,iNu2,iSub,iSeq) = corr(x, y);
+                    
+                    % Measure the mean squared difference between the two
+                    MseMat(iNu1,iNu2,iSub,iSeq) = log(mean((x - y) .^ 2));
+                end
             end
         end
     end
 end
 
-% Average over sequences
-AvgMat = cat(4, mean(CorMat, 4), mean(MseMat, 4));
-
-% Average over (pseudo-)subjects
-SemMat = squeeze(sem( AvgMat, 3));
-AvgMat = squeeze(mean(AvgMat, 3));
-
 %% DISPLAY SIMULATION RESULTS
 %  ==========================
 
-% Prepare a new window
-figure('Units', 'Normalized', 'Position', [0.35 0.3 0.3 0.35]);
+% Define colormaps to use
+cmap = cat(3, cbrewer2('Spectral', 101), flipud(cbrewer2('PuBuGn', 101)));
 
-% Define useful variables
-cmap = cat(3, cbrewer2('Spectral', 101), (cbrewer2('PuBuGn', 101)));
+% Define text labels
 labfun = @(x) sprintf('p(\\mathcal{M}_{\\mathrm{D}}|y,\\nu_{%1.0f})', x);
-cbrlab = {sprintf('$\\rho(%s,%s)$', labfun(1), labfun(2)), ...
-          sprintf('$-\\log((%s-%s)^{2})$', labfun(1), labfun(2))};
+cbrlab = {{'Average correlation coefficient', sprintf('$\\rho(%s,%s)$', labfun(1), labfun(2))}, ...
+          {'Average log-mean squared difference', sprintf('$-\\log((%s-%s)^{2})$', labfun(1), labfun(2))}};
 
-% For each matrix
-for iSp = 1:2
-    sp = subplot(1,2,iSp);
+% Prepare a new window
+figure('Position', [661 128 600 900])
+
+% For each type of sequence
+for iReg = 1:3
     
-    % Display the correlation matrix
-    imagesc(1:nNu, 1:nNu, AvgMat(:,:,iSp)); hold('on');
+    % Average over sequences
+    AvgMat = cat(4, mean(CorMat(:,:,:,cidx{iReg}), 4, 'OmitNaN'), ...
+                    mean(MseMat(:,:,:,cidx{iReg}), 4, 'OmitNaN'));
     
-    % Overlap coefficients
-    text(repmat(1:nNu, 1, nNu), sort(repmat(1:nNu, 1, nNu)), cellfun(@(x) ...
-        sprintf('%1.2f', x), num2cell(AvgMat(:,:,iSp)), ...
-        'UniformOutput', 0), 'HorizontalAlignment', 'Center', ...
-        'VerticalAlignment', 'Bottom', 'FontSize', 8);
-    text(repmat(1:nNu, 1, nNu), sort(repmat(1:nNu, 1, nNu)), cellfun(@(x) ...
-        sprintf('(%1.3f)', x), num2cell(SemMat(:,:,iSp)), ...
-        'UniformOutput', 0), 'HorizontalAlignment', 'Center', ...
-        'VerticalAlignment', 'Top', 'FontSize', 6);
+    % Average over (pseudo-)subjects
+    SemMat = squeeze(sem( AvgMat, 3));
+    AvgMat = squeeze(mean(AvgMat, 3));
     
-    % Customize the colormap
-    cbr = colorbar('Location', 'SouthOutside');
-    colormap(sp, flipud(cmap(:,:,iSp)));
-    if iSp == 1, caxis([-1,1]); end
-    
-    % Customize the axes
-    axis('square'); axis('xy'); set(gca, 'TickLabelInterpreter', 'LaTeX');
-    set(gca, 'XTick', 1:nNu, 'XTickLabel', cellfun(@(x) sprintf('%1.0f', x), ...
-        num2cell(Nu), 'UniformOutput', 0));
-    set(gca, 'YTick', 1:nNu, 'YTickLabel', get(gca, 'XTickLabel'));
-    
-    % Add some text labels
-    cbr.Label.Interpreter = 'LaTeX';
-    cbr.Label.String = cbrlab{iSp};
-    xlabel('$\nu_{1}$', 'Interpreter', 'LaTeX');
-    ylabel('$\nu_{2}$', 'Interpreter', 'LaTeX');
-    if     iSp == 1, t = title('Average correlation coefficients');
-    elseif iSp == 2, t = title('Average log-mean squared difference');
+    % For each matrix
+    for iSp = 1:2
+        sp = subplot(3,2,iSp+2*(iReg-1));
+        
+        % Display the correlation matrix
+        imagesc(1:nNu, 1:nNu, AvgMat(:,:,iSp)); hold('on');
+        
+        % Overlap coefficients as text labels
+        text(repmat(1:nNu, 1, nNu), sort(repmat(1:nNu, 1, nNu)), cellfun(@(x) ...
+            sprintf('%1.2f', x), num2cell(AvgMat(:,:,iSp)), ...
+            'UniformOutput', 0), 'HorizontalAlignment', 'Center', ...
+            'VerticalAlignment', 'Bottom', 'FontSize', 8);
+        text(repmat(1:nNu, 1, nNu), sort(repmat(1:nNu, 1, nNu)), cellfun(@(x) ...
+            sprintf('(%1.3f)', x), num2cell(SemMat(:,:,iSp)), ...
+            'UniformOutput', 0), 'HorizontalAlignment', 'Center', ...
+            'VerticalAlignment', 'Top', 'FontSize', 6);
+        
+        % Customize the colormap
+        colormap(sp, flipud(cmap(:,:,iSp)));
+        if iReg == 3
+            cbr = colorbar('Orientation', 'Horizontal');
+            pos = get(gca, 'Position');
+            set(cbr, 'Position', [pos(1), pos(2)-0.05, pos(3), 0.01]);
+        end
+        if iSp == 1, caxis([-1,1]); end
+        
+        % Customize the axes
+        axis('square'); axis('xy'); set(gca, 'TickLabelInterpreter', 'LaTeX');
+        set(gca, 'XTick', 1:nNu, 'XTickLabel', cellfun(@(x) sprintf('%1.0f', x), ...
+            num2cell(Nu), 'UniformOutput', 0));
+        set(gca, 'YTick', 1:nNu, 'YTickLabel', get(gca, 'XTickLabel'));
+        
+        % Add some text labels
+        if iReg == 3
+            cbr.Label.Interpreter = 'LaTeX';
+            cbr.Label.String = cbrlab{iSp};
+        end
+        xlabel('$\nu_{1}$', 'Interpreter', 'LaTeX');
+        ylabel('$\nu_{2}$', 'Interpreter', 'LaTeX');
+        t = title(sprintf('%s sequence', proclab{iReg}), 'Interpreter', 'LaTeX');
     end
-    set(t, 'Interpreter', 'LaTeX');
 end
