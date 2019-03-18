b96c81845ee7eab7c8715d9d2f16da6d502c968d
Maxime Maheu <maheu.mp@gmail.com>
2019-03-18 16:04:56 +0100

    New functions to shift & compare model/subjects

---
+++ Functions/Emergence_Shift.m
@@ -0,0 +1,25 @@
+function [ xp, yp ] = Emergence_Shift( x, y, d )
+% EMERGENCE_SHIFT shifts two arrays/matrices, one with respect to the
+% other.
+%   - "x": the first  matrix of Nobs*Ndim observations (e.g. subject)
+%   - "y": the second matrix of Nobs*Ndim observations (e.g. model)
+%   - "d": a scalar specifying the amount of shift (in terms of number of
+%       observations) of one with respect to the other.
+% 
+% Copyright (c) 2018 Maxime Maheu
+
+% Number of observations
+n = size(x,1);
+
+% If the subject is slower than the ideal observer
+if d >= 0
+    xp = x(d+1:n,:); % to be explained: subject's trajectory
+    yp = y(1:n-d,:); % explaining variable: IO's belief
+    
+% If the subject is faster than the ideal observer
+elseif d < 0
+    xp = x(1:n+d,:);  % to be explained: subject's trajectory
+    yp = y(-d+1:n,:); % explaining variable: IO's belief
+end
+
+end
---
+++ Functions/Emergence_Similarity.m
@@ -0,0 +1,120 @@
+function [ out, all, idx ] = Emergence_Similarity( pb, qb, metric, optim )
+% EMERGENCE_SIMILARITY returns similarity index between 2 matrices of
+% posterior probability.
+%   - "pb": first N*3 matrix of posterior probability.
+%   - "qb": second N*3 matrix of posterior probability.
+%   - "metric": a string of characters specifying what to return:
+%       - "P*": Pearson correlation coefficient...
+%       - "D*": Euclidean distance...
+%       - "U*": Univariate log-likelihood...
+%       - "M*": Multivariate log-likelihood...
+%       - "*B": ...on Barycentric coordinates
+%       - "*C": ...on Cartesian coordinates
+%   - "optim": either a boolean specifying whether to optimize for temporal
+%       delay or an array of integers specifying the temporal delays over
+%       which to optimize (in terms of observation #, see Emergence_Shift).
+% 
+% Copyright (c) 2018 Maxime Maheu
+
+% Make sure the variables are N*3 matrices
+if isstruct(pb), pb = pb.BarycCoord; end
+if isstruct(qb), qb = qb.BarycCoord; end
+if any(size(pb) ~= size(qb)), error('Inputs must have the same size'); end
+N = size(pb,1);
+
+% Which metric to use
+if nargin < 3 || isempty(metric), metric = 'MC'; end
+checkfun = @(x) contains(metric, x, 'IgnoreCase', true);
+
+% Convert to cartesian coordinates
+if checkfun('C')
+    tcn = [0, sqrt(3)/2; 1, sqrt(3)/2; 1/2, 0];
+    pc = pb * tcn;
+    qc = qb * tcn;
+end
+
+% Whether to optimize for temporal delay
+if nargin < 4, optim = false; end
+if islogical(optim)
+    if      optim, dlist = 0:N;
+    elseif ~optim, dlist = 0; 
+    end
+elseif isnumeric(optim)
+    dlist = optim;
+    if any(dlist < -N | dlist > N), error('Check the shift array.'); end
+end
+nd = numel(dlist);
+
+% Prepare output variable
+all = NaN(nd,1);
+
+% Shift one with respect to the other
+for d = 1:nd
+    curd = dlist(d);
+    if     checkfun('C'), [p,q] = Emergence_Shift(pc, qc, curd);
+    elseif checkfun('B'), [p,q] = Emergence_Shift(pb, qb, curd);
+    end
+    n = N - abs(curd); % number of remaining observations
+    
+    % Pearson correlation coefficient
+    % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    if checkfun('P')
+        p = p(:); % column vector
+        q = q(:); % column vector
+        mp = sum(p) / n; % average (over observations and dimensions)
+        mq = sum(q) / n; % average (over observations and dimensions)
+        all(d) = sum((p - mp) .* (q - mq)) / ... % correlation coefficient
+            (sqrt(sum((p - mp) .^ 2)) * sqrt(sum((q - mq) .^ 2)));
+        
+    % Mean squared error
+    % ~~~~~~~~~~~~~~~~~~
+    elseif checkfun('E')
+        e = p - q; % signed error
+        se = e.^2; % squared error
+        sse = sum(se, 2); % sum of squared error (over dimensions)
+        all(d) = mean(sse); % average squared error (over observations)
+        
+    % Euclidean distance
+    % ~~~~~~~~~~~~~~~~~~
+    elseif checkfun('D')
+        e = p - q; % signed error
+        se = e.^2; % squared error
+        sse = sum(se, 2); % sum of squared error (over dimensions)
+        ed = sqrt(sse); % euclidean distance
+        all(d) = mean(ed); % average euclidean distance (over observations)
+        
+    % Log-likelihood under univariate normally distributed errors
+    % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    elseif checkfun('U')
+        p = p(:); % column vector
+        q = q(:); % column vector
+        e = p - q; % signed error
+        s2 = (1/n) * sum(e.^2); % variance with mu = 0
+        all(d) = - n/2 * log(2*pi) - n/2 * log(s2) - 1/(2*s2) * sum(e.^2);
+        % log-likelihood of the univariate distance
+        
+    % Log-likelihood under multivariate normally distributed errors
+    % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    elseif checkfun('M')
+        e = p - q; % signed error
+        k = size(e, 2); % number of dimensions
+        S = (1/n) * (e' * e); % covariance matrix
+        all(d) = -1/2 * sum(log(det(S)) + sum(e * inv(S) .* e, 2) + k * log(2*pi));
+        % log-likelihood of the bivariate distance
+        % N.B. It is equivalent to the univariate case when e is a vector, 
+        % and thus k = 1.
+    end    
+end
+
+% Whether to find the maximum or the minimum over shifts
+if     checkfun('P'), funtooptim = @max; % maximum correlation coefficient
+elseif checkfun('E'), funtooptim = @min; % minimum error
+elseif checkfun('D'), funtooptim = @min; % minimum distance
+elseif checkfun('U'), funtooptim = @max; % maximum log-likelihood
+elseif checkfun('M'), funtooptim = @max; % maximum log-likelihood
+end
+    
+% Find the best temporal shift
+[out,idx] = funtooptim(all);
+
+end
