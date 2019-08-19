188c7994f6830d80bf891cc6b2a8e3b26cfc1c37
Maxime <maxime.maheu@live.fr>
2019-08-19 22:10:26 +0200

    Add option to restrict to deteted regularities

--- Finger tracking analyses/Emergence_MC_Dynamics.m
+++ Finger tracking analyses/Emergence_MC_Dynamics.m
@@ -4,31 +4,43 @@
 % 
 % Copyright (c) 2018 Maxime Maheu
 
+% Run simulations
+% ~~~~~~~~~~~~~~~
+
 % Get model simulations
 SimuType = 'IndependentDifferenceDiscrete';
 Emergence_MC_ModelSimulations;
 
+% Define options
+% ~~~~~~~~~~~~~~
+
 % Define the type of plot
 plttype = 'dyn'; % 'tri' or 'dyn'
 
-% Whether to display 
+% Whether to restrict to sequences that were accurately classified by
+% subjects
+onlydetreg = true;
+
+% Whether to display the average sequence
 dispseq = false;
 
-% Display the other hypothesis
+% Display the other (incorrect) hypotheses
 dispalt = false;
 
 % Window around change point to look into (in number of observations)
 ObsWin = 0:60;
-n = numel(ObsWin);
+nObs = numel(ObsWin);
+
+% Display detection dynamics
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 % Define positions of figures' subplots
-SpPos = {[6,7,8,10,11,12,14,15,16,2,1], ...
-         [1,6,10,14,7,11,3,8,12,16]};
+SpPos = {[6,7,8,10,11,12,14,15,16,2,1], [1,6,10,14,7,11,3,8,12,16]};
 
 % Prepare output variables
-Seq = cell(1,2);
-AvgIO = cell(1,2);
-ErrIO = cell(1,2);
+AvgSeq = cell(1,2);
+AvgPP  = cell(1,2);
+ErrPP  = cell(1,2);
 
 % For each type of regularity
 for iReg = 1:2
@@ -37,19 +49,28 @@ for iReg = 1:2
     cp = cellfun(@(x) x.Jump + 1/2, G(cidx{iReg},:), 'uni', 0);
     
     % Select sequence likelihood around change point's position
-    WinIO = cellfun(@(p,c) Emergence_LockOnPoint(p,c,ObsWin), ...
+    PP = cellfun(@(p,c) Emergence_LockOnPoint(p,c,ObsWin), ...
         pMgY(cidx{iReg},:,:), repmat(cp, [1,1,nMod]), 'uni', 0);
-    WinIO = cell2mat(cellfun(@(x) reshape(x,[1,1,1,n,3]), WinIO, 'uni', 0));
+    PP = cell2mat(cellfun(@(x) reshape(x,[1,1,1,nObs,3]), PP, 'uni', 0));
     
     % Get average sequence
-    seq = cellfun(@(x,i) Emergence_LockOnPoint(x.Seq',i,ObsWin), ...
+    Seq = cellfun(@(x,i) Emergence_LockOnPoint(x.Seq',i,ObsWin), ...
         G(cidx{iReg},:), cp, 'uni', 0);
-    seq = cell2mat(cellfun(@(x) reshape(x,[1,1,n]), seq, 'uni', 0));
-    Seq{iReg} = squeeze(round(mean(seq, 2, 'OmitNaN')));
+    Seq = cell2mat(cellfun(@(x) reshape(x,[1,1,nObs]), Seq, 'uni', 0));
+    
+    % Remove regularities that were not identified by subjects
+    if onlydetreg
+        detecmask = (filter{2} == 3);
+        nanidx = repmat(~detecmask, [1,1,nMod,nObs,3]);
+        PP(nanidx) = NaN;
+        nanidx = repmat(~detecmask, [1,1,nObs]);
+        Seq(nanidx) = NaN;
+    end
     
     % Average over (pseudo-)subjects
-    AvgIO{iReg} = squeeze(mean(WinIO, 2, 'OmitNaN'));
-    ErrIO{iReg} = squeeze(sem(WinIO, 2));
+    AvgSeq{iReg} = squeeze(round(mean(Seq, 2, 'OmitNaN')));
+    AvgPP{iReg}  = squeeze(mean(PP, 2, 'OmitNaN'));
+    ErrPP{iReg}  = squeeze(sem(PP, 2));
     
     % Prepare the window
     figure('Position', [200+760*(iReg-1) 565 760 540]);
@@ -68,8 +89,8 @@ for iReg = 1:2
         for iMod = [fliplr(1:nMod-1), nMod]
             
             % Get posterior likelihood in each hypothesis
-            y = squeeze(AvgIO{iReg}(iSeq,iMod,:,:));
-            e = squeeze(ErrIO{iReg}(iSeq,iMod,:,:));
+            y = squeeze(AvgPP{iReg}(iSeq,iMod,:,:));
+            e = squeeze(ErrPP{iReg}(iSeq,iMod,:,:));
             
             % Define the color to use
             c = modc(iMod,:);
@@ -84,7 +105,8 @@ for iReg = 1:2
                 % Overlap average sequence
                 if dispseq
                     obscol = [c; ones(1,3)];
-                    s = scatter(y(:,1), y(:,2), 15, obscol(Seq{iReg}(iSeq,:),:), 'filled');
+                    s = scatter(y(:,1), y(:,2), 15, ...
+                        obscol(AvgSeq{iReg}(iSeq,:),:), 'Filled');
                     set(s, 'MarkerEdgeColor', c);
                 end
                 
@@ -107,7 +129,7 @@ for iReg = 1:2
         if strcmpi(plttype, 'dyn') && dispseq
             obscol = {'k'; 'w'};
             for i = 1:2
-                idx = Seq{iReg}(iSeq,:) == i;
+                idx = AvgSeq{iReg}(iSeq,:) == i;
                 plot(ObsWin(idx), repmat(i-1, [1,sum(idx)]), 'o', ...
                     'MarkerEdgeColor', 'k', 'MarkerFaceColor', ...
                     obscol{i}, 'MarkerSize', 5);
@@ -118,7 +140,7 @@ for iReg = 1:2
         if     iReg == 1, title(pr{iSeq});
         elseif iReg == 2, title(dr{iSeq});
         end
-        
+        set(gca, 'XTick', [], 'YTick', [], 'box', 'off'); title('');
         % Customize the axes
         if     strcmpi(plttype, 'dyn'), axis([ObsWin([1,end]),0,1]); 
         elseif strcmpi(plttype, 'tri'), axis([-1,1,0,sqrt(3)/2]);
