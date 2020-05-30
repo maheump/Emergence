1209aa3471df6dad6bd792fb3d491772267799e5
Maxime Maheu <maheu.mp@gmail.com>
2020-05-30 17:50:45 +0200

    Major update: add a new set of models
    - Update the help section.
    - Add new model which uses a learning of TPs of the same order for both non-random hypotheses, but different priors (flat for stat. vs. biased for rules).
    - Remove the duplicate model (n = 10) for simulations varying the depth of the tree to explore in the deterministic rule hypothesis.
    - Unify the colormaps.
    - Improve and dhorten the last part of the script which combine simulations with the full ideal inference.

--- Finger tracking analyses/Emergence_MC_ModelSimulations.m
+++ Finger tracking analyses/Emergence_MC_ModelSimulations.m
@@ -1,19 +1,22 @@
 % This script runs simulations of the alternative ideal observed models. In
 % particular we constrast the repeating pattern detector to various Markov
 % chains of different orders. The simulations are run on sequences that
-% were presented to the subjects.
-% 
-% The different alternative models are the following ones:
+% were presented to the subjects. We considered several alternatives.
+%
+% NORMATIVE SINGLE-SYSTEM MODEL (i.e. direct comparison between statistics
+% and rules, but same hypothesis space relying on Markov chains of more or
+% less high order)
 %   - "PseudoDeterministic": replaces the deterministic hypothesis with a
 %       probabilistic hypothesis learning higher-order transition
 %       probabilities.
 %   - "BiasedPseudoDeterministic": same as the previous one but in addition
 %       uses a prior biases for predictable cases.
-%   - "Leak": uses an exponential leak when counting observations in the
-%       case of the probabilistic hypothesis.
-%   - "TreeDepth": uses a deterministic hypothesis that considers different
-%       maximum pattern length than the one of the longest pattern (i.e.
-%       10) used in the experiment.
+%   - "DifferentPriors": probabilistic and deterministic hypotheses use the
+%       same Markov chain (we vary the order) but with different prior
+%       distribution (respectively biased vs. flat).
+%
+% NON-COMMENSURATE TWO-SYSTEM MODEL (i.e. distinct hypothesis spaces for
+% statistics and rules, but no direct comparison)
 %   - "IndependentDiffContinuous": uses a sigmoid on the difference between
 %       independently-computed likelihoods in the regular hypotheses in
 %       order to combine them (instead of the rules of probability).
@@ -22,8 +25,15 @@
 %   - "IndependentRatioContinuous": uses a sigmoid on the log-ratio between
 %       independently-computed likelihoods in the regular hypotheses in
 %       order to combine them (instead of the rules of probability).
-%   - "IndependentRatioDiscrete": uses example extreme cases of the previous
-%       one (i.e. a max, linear and example sigmoid versions).
+%   - "IndependentRatioDiscrete": uses example extreme cases of the
+%       previous one (i.e. a max, linear and example sigmoid versions).
+%
+% OTHER ALTERNATIVES:
+%   - "TreeDepth": uses a deterministic hypothesis that considers different
+%       maximum pattern length than the one of the longest pattern (i.e.
+%       10) used in the experiment.
+%   - "Leak": uses an exponential leak when counting observations in the
+%       case of the probabilistic hypothesis.
 % 
 % Copyright (c) 2018 Maxime Maheu
 
@@ -77,7 +87,31 @@ if strfun('PseudoDeterministic')
         options(lidx,:) = arrayfun(@(x) repmat(999/1000, 2, 2^x), orders, 'uni', 0);
     end
     
-% SIMU #3: Alternative probabilistic hypothesis assuming a local integration
+% SIMU #3: Alternative probabilistic and deterministic hypotheses which
+% both estimate (low to high-order) transition probabilities but using
+% respectively a prior flat or biased for predictive cases
+% ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+elseif strfun('DifferentPriors')
+    
+    % Define models to test
+    orders = repmat(1:9, 1, 2);
+    nMod = numel(orders);
+    
+    % Replicate the default options for all the models to simulate
+    options = struct2cell(defo);
+    options = repmat(options, 1, nMod);
+    
+    % Change the statistics to learn for these models
+    lidx = strcmpi(lab, 'stat');
+    options(lidx,:) = arrayfun(@(x) sprintf('Chain%1.0f', x), orders, 'uni', 0);
+    
+    % Half of the models use a flat prior and the remaining half use a
+    % prior biased for predictability
+    lidx = strcmpi(lab, 'pT');
+    options(lidx,nMod/2+1:end) = arrayfun(@(x) repmat(999/1000, 2, 2^x), orders(nMod/2+1:end), 'uni', 0);
+    
+    
+% SIMU #4: Alternative probabilistic hypothesis assuming a local integration
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 elseif strfun('Leak')
     
@@ -111,12 +145,12 @@ elseif strfun('Leak')
     % For the ultimate model (no error), use analytical solutions
     options(lidx,end) = {[]};
     
-% SIMU #4: Alternative deterministic hypothesis with different tree depth
+% SIMU #5: Alternative deterministic hypothesis with different tree depth
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 elseif strfun('TreeDepth')
     
     % Define depth of trees to explore
-    nu = [4 6 8 20 50 N 10];
+    nu = [4 6 8 20 50 N];
     nMod = numel(nu);
     
     % Replicate the default options for all the models to simulate
@@ -127,7 +161,7 @@ elseif strfun('TreeDepth')
     lidx = strcmpi(lab, 'patlen');
     options(lidx,:) = arrayfun(@(x) x, nu, 'uni', 0);
     
-% SIMU #5: Alternative (non-normative) weighting of the regular hypotheses
+% SIMU #6: Alternative (non-normative) weighting of the regular hypotheses
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 elseif strfun('Independent')
     
@@ -196,7 +230,7 @@ catch
             
             % For each condition
             for iSeq = 1:nSeq
-                fprintf('- Running the IO on sequence #%2.0f/%2.0f from subject #%2.0f/%2.0f.\n', ...
+                fprintf('- Running models on sequence #%2.0f/%2.0f from subject #%2.0f/%2.0f.\n', ...
                     iSeq, nSeq, iSub, nSub);
                 
                 % Get the sequence
@@ -225,13 +259,12 @@ end
 %% COMPUTE HYPOTHESIS LIKELIHOOD
 %  =============================
 
-% For simulations with high-order Markov chains use them as
-% pseudo-deterministic hypothesis, not as a higher-order probabilistic
-% hypothesis
+% Get sequence likelihood for the ideal observer model
+fullIO = cellfun(@(x) x.SeqLLH, IO, 'uni', 0);
+
+% For simulations with a pseudo-deterministic hypothesis which uses higher-
+% order Markov chains
 if strfun('PseudoDeterministic')
-    
-    % Get sequence likelihood for the ideal observer model
-    fullIO = cellfun(@(x) x.SeqLLH, IO, 'uni', 0);
     
     % Get sequence likelihood under different hypotheses
     pYgMsp = cellfun(@(x) x(:,1), fullIO, 'uni', 0); % 1st-order Markov chain
@@ -245,22 +278,47 @@ if strfun('PseudoDeterministic')
         repmat(pYgMsp, [1,1,nMod]), pYgMsc, repmat(pYgMss, [1,1,nMod]), ...
         'uni', 0);
     
-    % Append the fully ideal observer model
-    mIO = cat(3, mIO, fullIO);
-    options = cat(2, options, struct2cell(defo));
+    % Attribute a color to each model
+    modc = flipud(winter(nMod));
+    
+% For simulations with same low- to high-order Markov chains for both
+% probabilistic and deterministic hypotheses but with different priors
+% (respectively flat and biased)
+elseif strfun('DifferentPriors')
+    
+    % Get simulations that use either a flat or biased prior
+    lidx = strcmpi(lab, 'pT');
+    unifprior = strcmpi(options(lidx,:), 'Bayes-Laplace');
+    biasprior = ~unifprior;
+    nMod = sum(unifprior);
+    
+    % Get sequence likelihood
+    pYgMsp = cellfun(@(x) x(:,1), mIO(:,:,unifprior), 'uni', 0); % pseudo-probabilistic
+    pYgMsd = cellfun(@(x) x(:,1), mIO(:,:,biasprior), 'uni', 0); % pseudo-deterministic
+    pYgMss = repmat(cellfun(@(x) x(:,3), fullIO, 'uni', 0), [1,1,nMod]); % fully-stochastic
+    
+    % Combine sequence likelihood under each hypothesis
+    mIO = cellfun(@(pH,pD,pS) cat(2,pH,pD,pS), pYgMsp, pYgMsd, pYgMss, 'uni', 0);
+    
+    % Adapt options to reflect modified simulations
+    lidx1 = strcmpi(lab, 'patlen');
+    lidx2 = strcmpi(lab, 'stat');
+    options(lidx1,unifprior) = options(lidx2,biasprior);
+    lidx1 = strcmpi(lab, 'pR');
+    lidx2 = strcmpi(lab, 'pT');
+    options(lidx1,unifprior) = options(lidx2,biasprior);
+    options = options(:,unifprior);
     
     % Attribute a color to each model
-    if strfun('Biased'), modc = [flipud(autumn(nMod)); zeros(1,3)];
-    else,                modc = [flipud(winter(nMod)); zeros(1,3)];
-    end
+    modc = flipud(winter(nMod));
 
 % For stimulations with different maximum pattern length allowed
 elseif strfun('TreeDepth')
-    modc = [flipud(Emergence_Colormap('Reds', nMod-1)); zeros(1,3)];
+    modc = flipud(Emergence_Colormap('Reds', nMod));
     
 % For stimulations with exponential leak
 elseif strfun('Leak')
-    modc = [flipud(winter(nMod-1)); zeros(1,3)];
+    modc = flipud(autumn(nMod));
     
 % For simulations with independent (non rational) combination of hypothesis
 % likelihood
@@ -301,7 +359,7 @@ elseif strfun('Independent')
                 elseif slope > 0
                     if strfun('Ratio') % log ratio between regular hypothesis likelihoods
                         Wp = 1 ./ (1 + exp(-slope .* log(qHpgY ./ qHdgY)));
-                    elseif strfun('Difference') % difference between regular hypothesis likelihoods
+                    elseif strfun('Diff') % difference between regular hypothesis likelihoods
                         Wp = 1 ./ (1 + exp(-slope .* (qHpgY - qHdgY)));
                     end
                 end
@@ -315,23 +373,31 @@ elseif strfun('Independent')
         end
     end
     
-    % Concatenate results
+    % Append the fully ideal observer model
     pMgY = cat(3, mIO, cellfun(@(x) x.BarycCoord, IO, 'uni', 0));
+    options = cat(2, options, cat(1, struct2cell(defo), 'Rational'));
     
-    % Define the colors to use
-    if     strfun('Ratio'),      modc = [winter(nMod); zeros(1,3)];
-    elseif strfun('Difference'), modc = [cool(nMod);   zeros(1,3)];
-    end
+    % Attribute a color to each model
+    modc = cool(nMod);
 end
 
-% Compute posterior probability over hypotheses
+% If the posterior probabilities have not yet been computed
 if ~strfun('Independent')
+    
+    % Append the fully ideal observer model
+    mIO = cat(3, mIO, fullIO);
+    options = cat(2, options, struct2cell(defo));
+    
+    % Compute posterior probability over hypotheses
     pMgY = cellfun(@(x) exp(x) ./ sum(exp(x), 2), mIO, 'uni', 0);
 end
+
+% Wrap things up
+modc = [modc; zeros(1,3)];
 nMod = size(pMgY, 3);
 
 % Make all trajectories to start at the bottom tip of the triangle
 for i = 1:numel(pMgY), pMgY{i}(1,:) = [0,0,1]; end
 
-% Remove datasets
+% Remove useless datasets (i.e. sequence likelihoods)
 clear('mIO');
