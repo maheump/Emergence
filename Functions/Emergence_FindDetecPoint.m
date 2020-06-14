09b1ca9d2e2fa6380df3052b0aef9e357ec5288f
Maxime Maheu <maheu.mp@gmail.com>
2020-06-14 23:03:08 +0200

    Add option to use different threshold for detection

--- Functions/Emergence_FindDetecPoint.m
+++ Functions/Emergence_FindDetecPoint.m
@@ -1,4 +1,4 @@
-function dp = Emergence_FindDetecPoint(bel)
+function dp = Emergence_FindDetecPoint( bel, thr )
 % EMERGENCE_FINDDETECPOINT returns position of the detection point.
 %   - "bel": the 1xN array of posterior probabilities as a function of
 %       time.
@@ -6,16 +6,16 @@ function dp = Emergence_FindDetecPoint(bel)
 % Copyright (c) 2020 Maxime Maheu
 
 % The belief threshold to exceed in order to "detect" a regularity
-threshold = 1/2;
+if nargin < 2, thr = 1/2; end
 
 % CONSTRAINT 1: larger than threshold
-idx1 = (bel >= threshold);
+idx1 = (bel >= thr);
 
 % CONSTRAINT 2: it should cross the threshold in a positive manner
 % i.e. the one before should be smaller than the threshold
 % CONSTRAINT 3: it should not be already above theshold at the time of
 % change point
-idx2 = [false; (bel(1:end-1) < threshold)];
+idx2 = [false; (bel(1:end-1) < thr)];
 logicdet = (idx1 & idx2);
 
 % The detection point is the first threshold crossing
