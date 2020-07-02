813cff99083d5b8115847db34d82029c8b1810a2
Maxime Maheu <maheu.mp@gmail.com>
2020-07-02 21:32:31 +0200

    Improve the readout catagorisation function
    - make the function more general
    - account for indecision between the 3 hypotheses (not simply the 2 non-random ones)

--- Functions/Emergence_GetModelCategorisation.m
+++ Functions/Emergence_GetModelCategorisation.m
@@ -1,10 +1,10 @@
-function [ dist ] = Emergence_GetModelCategorisation( pMgY, prec )
+function [ EstGenProc, dist, odist ] = Emergence_GetModelCategorisation( pMgY, prec )
 % EMERGENCE_GETMODELCATEGORISATION returns the most likely hypothesis
 % given the set of posterior probabilities over hypotheses. The function
 % accounts for possible indecision cases between pairs of hypotheses by
 % splitting the counts and attributing it in half/half proportion to each
 % of the two hypotheses.
-%   - "pMgY: a Nx3 matrix (where N is oftten the number of subjects)
+%   - "pMgY: a Nx3 matrix (where N is often the number of subjects)
 %       specifying the posterior over hypotheses for a set of sequences.
 % 
 % Copyright (c) 2020 Maxime Maheu
@@ -24,45 +24,55 @@ EGP2 = idx(:,end-1); % second
 EstGenProc = EGP1;
 MAP = val(:,end);
 
-% Get indecision cases
-if nargin < 2, prec = 0.01; end
-Indecision = find(MAP >= (1/2 - prec) & MAP <= (1/2 + prec));
-
-% Label the indecision cases
-Nindec = numel(Indecision);
+% Deal with indecision between 2 hypotheses
+if nargin < 2, prec = 0.001; end
+Indecision2 = find(MAP >= (1/2 - prec) & MAP <= (1/2 + prec));
+Nindec = numel(Indecision2);
 for i = 1:Nindec
-    i = Indecision(i);
-    if     (EGP1(i) == 1 & EGP2(i) == 2), EstGenProc(i) = 4; % stat/rule
-    elseif (EGP1(i) == 2 & EGP2(i) == 1), EstGenProc(i) = 4; % stat/rule
+    i = Indecision2(i);
+    if     (EGP1(i) == 2 & EGP2(i) == 3), EstGenProc(i) = 4; % rule/rand
+    elseif (EGP1(i) == 3 & EGP2(i) == 2), EstGenProc(i) = 4; % rule/rand
     elseif (EGP1(i) == 1 & EGP2(i) == 3), EstGenProc(i) = 5; % stat/rand
     elseif (EGP1(i) == 3 & EGP2(i) == 1), EstGenProc(i) = 5; % stat/rand
-    elseif (EGP1(i) == 2 & EGP2(i) == 3), EstGenProc(i) = 6; % rule/rand
-    elseif (EGP1(i) == 3 & EGP2(i) == 2), EstGenProc(i) = 6; % rule/rand
+    elseif (EGP1(i) == 1 & EGP2(i) == 2), EstGenProc(i) = 6; % stat/rule
+    elseif (EGP1(i) == 2 & EGP2(i) == 1), EstGenProc(i) = 6; % stat/rule
     end
 end
 
-% Get the frequency of each 
-dist = histc(EstGenProc, 1:6);
+% Deal with indecision between 3 hypotheses
+Indecision3 = all(MAP >= (1/3 - prec) & MAP <= (1/3 + prec), 2);
+EstGenProc(Indecision3) = 7;
 
-% Indecision between statistical bias and deterministic rule
-for i = 4:6
-    Nindec = dist(i) ./ 2;
-    if i == 4
-        dist(1) = dist(1) + Nindec; % statistical bias
-        dist(2) = dist(2) + Nindec; % deterministic rule
-    elseif i == 5
-        dist(1) = dist(1) + Nindec; % statistical bias
-        dist(3) = dist(3) + Nindec; % fully random
-    elseif i == 6
-        dist(2) = dist(2) + Nindec; % deterministic rule
-        dist(3) = dist(3) + Nindec; % fully random
+% If we have to export the distribution
+if nargout > 1
+    
+    % Get the frequency of each 
+    odist = histc(EstGenProc, 1:7);
+    
+    % Indecision between statistical bias and deterministic rule
+    for i = 4:7
+        if     i  < 7, Nindec = odist(i) ./ 2; % between 2 hypotheses
+        elseif i == 7, Nindec = odist(i) ./ 3; % between 3 hypotheses
+        end
+        if i == 4
+            odist(2) = odist(2) + Nindec; % deterministic rule
+            odist(3) = odist(3) + Nindec; % fully random
+        elseif i == 6
+            odist(1) = odist(1) + Nindec; % statistical bias
+            odist(2) = odist(2) + Nindec; % deterministic rule
+        elseif i == 5
+            odist(1) = odist(1) + Nindec; % statistical bias
+            odist(3) = odist(3) + Nindec; % fully random
+        elseif i == 7
+            odist = odist + Nindec; % all 3 hypotheses
+        end
     end
+    
+    % Remove indecision cases
+    dist = odist(1:3);
+    
+    % Normalize such that the distribution sums to 1
+    dist = dist ./ nSub;
 end
-
-% Remove indecision cases
-dist = dist(1:3);
-
-% Normalize such that the distribution sums to 1
-dist = dist ./ nSub;
 
 end
